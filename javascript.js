b8ba62eaa88a5001838c80d162f30b2c5c391a03
ritika172 <bailkwadritika@gmail.com>
2024-10-11 12:43:26 +0530

    change the code

--- javascript.js
+++ javascript.js
@@ -18,11 +18,3 @@ for(i=0;i<myNodelist.length;i++){
     span.appendChild(txt);
     myNodelist[i].appendChild(span);
 }
-var close=document.getElementsByClassName("close");
-var i;
-for(i=0;i<close.length;i++){
-close[i].onclick=function(){
-    var div=this.parentElement;
-    div.style.display="none";
-};
-}
